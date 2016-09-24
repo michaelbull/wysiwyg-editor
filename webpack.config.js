009114467eaef5c6da92c6121345de639421164f
Michael Bull <mikebull94@gmail.com>
2016-09-24 20:06:41 +0100

    Add minified distribution support

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: './src/index.jsx',
 
@@ -32,3 +34,21 @@ module.exports = {
         ]
     }
 };
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins = [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.OccurenceOrderPlugin()
+    ];
+} else {
+    module.exports.devtool = 'source-map';
+}
